061175f296a34ea54ba4ca752b452c71f688097c
Francisco Rutayebesibwa <fmruta98@gmail.com>
2021-09-23 12:30:28 +0200

    chore:Fix MySQL statement in category python file.

--- app/category.py
+++ app/category.py
@@ -10,17 +10,20 @@ from flask_cors import cross_origin
 def getAvailableCategory():
     query_parameters = request.args
     print(query_parameters)
-    category = query_parameters.get('CategoryName')
-    print(category)
-    print('Printing query')
+    category = query_parameters.get('categoryName')
+    #print(category)
+    #print('Printing query')
     
     query = """
-    SELECT A.Title, A.URL, A.DOI, A.YearPublished, J.JournalName, C.CategoryName FROM Articles AS A, Journals AS J, Category AS C
-    WHERE C.CategoryName='{0}'
+    SELECT A.Title, A.URL, A.DOI, A.YearPublished, J.JournalName, C.CategoryName FROM Articles 
+    AS A, Journals AS J, Category AS C 
+    WHERE  A.JournalID = J.JournalID AND A.CategoryID = C.CategoryID AND 
+    C.CategoryName='{0}'
+    ORDER BY A.YearPublished DESC;
     """.format(
         category
     )
-    print(query)
+    #print(query)
     conn = mysql.connect()
     cursor = conn.cursor(pymysql.cursors.DictCursor)
     cursor.execute(query)
