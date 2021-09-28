7cc979a4d913bfe3b804c25ff5a6de3a32706cc3
Francisco Rutayebesibwa <fmruta98@gmail.com>
2021-09-28 11:18:29 +0200

    chore: Make code neater and remove comments

--- app/journal.py
+++ app/journal.py
@@ -4,7 +4,7 @@ from db import mysql
 from flask import jsonify, request
 from flask_cors import cross_origin
 
-# Get list of journals
+
 @app.route("/api/journals/titles")
 @cross_origin()
 def getAvailableJournals():
@@ -23,7 +23,6 @@ def getAvailableJournals():
 
     return resp
 
-#Get all articles of a specific journal
 @app.route("/api/journals")
 @cross_origin()
 def getSpecificArticlesfromJournals():
@@ -31,10 +30,27 @@ def getSpecificArticlesfromJournals():
     journal = query_parameters.get('journalName')
 
     query = """
-    SELECT Title, URL, DOI, YearPublished, JournalName, CategoryName, AuthorInitial, AuthorSurname
-    FROM (SELECT * FROM (SELECT A.ArticleID, A.Title, A.URL, A.DOI, A.YearPublished, J.JournalName, 
-    C.CategoryName FROM Articles AS A, Journals AS J, Category AS C WHERE A.JournalID = J.JournalID 
-    AND A.CategoryID = C.CategoryID ORDER BY A.YearPublished DESC) AS New_View) AS J_C_A INNER JOIN Authors
+    SELECT Title,
+    URL, 
+    DOI, 
+    YearPublished,
+    JournalName,
+    CategoryName,
+    AuthorInitial, 
+    AuthorSurname
+    
+    FROM 
+    (SELECT * FROM 
+    
+    (SELECT A.ArticleID, A.Title, A.URL, A.DOI, A.YearPublished, J.JournalName, 
+    C.CategoryName FROM Articles AS A, Journals AS J, Category AS C 
+    WHERE A.JournalID = J.JournalID 
+    AND A.CategoryID = C.CategoryID 
+    ORDER BY A.YearPublished DESC)
+    
+    AS New_View)
+    
+    AS J_C_A INNER JOIN Authors
     On J_C_A.ArticleID = Authors.AuthorID WHERE JournalName = '{0}'
     ORDER BY YearPublished DESC;
     """.format(
