400c18f90e5b63f8f8784b502f9965b32034a2bb
Francisco Rutayebesibwa <fmruta98@gmail.com>
2021-10-11 11:47:47 +0200

    feat: create doi endpoint

---
+++ app/DOI.py
@@ -0,0 +1,31 @@
+import pymysql
+from app import app
+from db import mysql
+from flask import jsonify, request
+from flask_cors import cross_origin
+
+@app.route("/api/articles/doi")
+@cross_origin()
+def checkArticlebyDOI():
+    query_parameters = request.args
+    search = query_parameters.get('checkdoi')
+
+    query = """
+    SELECT DOI
+    FROM Articles
+    WHERE DOI ="{}";
+    """.format(
+        search
+    )
+    
+    conn = mysql.connect()
+    cursor = conn.cursor(pymysql.cursors.DictCursor)
+    cursor.execute(query)
+    results = cursor.fetchall()
+
+    resp = jsonify(results)
+
+    resp.status_code = 200
+
+    return resp
+
--- app/rest.py
+++ app/rest.py
@@ -7,3 +7,4 @@ import articles
 import category
 import journal
 import authors
+import DOI
